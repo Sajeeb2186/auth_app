e923e456b1d2b60a4c4c65bf2e5d0afda19632a8
Sajeeb <sajeeb2186@gmail.com>
2025-03-10 12:56:35 +0600

    auth app initialization

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/LoginForm";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+       <LoginForm></LoginForm>
+       
+  );
+}
---
+++ components/LoginForm.jsx
@@ -0,0 +1,7 @@
+
+
+export default function LoginForm() {
+  return (
+    <div className="gird  place-item-center  h-screen ">LoginForm</div>
+  )
+}
