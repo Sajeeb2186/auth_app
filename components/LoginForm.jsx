ddadf972955a6b0dcc00c44110a5b2afbda077fd
Sajeeb <sajeeb2186@gmail.com>
2025-03-13 15:14:21 +0600

    login form create

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -2,6 +2,20 @@
 
 export default function LoginForm() {
   return (
-    <div className="gird  place-item-center  h-screen ">LoginForm</div>
+    <div className="grid  place-items-center  h-screen ">
+
+
+      <div className="shadow p-5 rounder-lg border-t-4 border-green-400">
+        <h1 className="text-xl font-bold my-4">Enter the Details</h1>
+        <form className="flex flex-col gap-3">
+          <input type="text" name="" id="" placeholder="Email" />
+          <input type="password" name="" id="" placeholder="Password" />
+
+          <button className="bg-green-600 text-white font-bold   cursor-pointer px-6 py-2">Login</button>
+          
+
+        </form>
+      </div>
+     </div>
   )
 }
